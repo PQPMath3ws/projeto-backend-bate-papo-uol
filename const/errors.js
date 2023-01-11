46fd64d41377a7f8b18c98f948d04cdf3e803fbb
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-01-11 09:27:15 -0300

    Implementação inicial de erro de método nas rotas participants e messages

--- const/errors.js
+++ const/errors.js
@@ -2,6 +2,7 @@ const setError = (code, message) => ({code, message});
 
 const errors = {
     404: setError(404, "Rota não encontrada na API do servidor!"),
+    405: setError(405, "Método não permitido!"),
 };
 
 export default errors;
--- routes/errors.js
+++ routes/errors.js
@@ -4,6 +4,14 @@ import errors from "../const/errors.js";
 
 const router = express.Router();
 
+router.all("/participants", (req, res) => {
+    if (req.method !== "POST") return res.status(errors[405].code).send(errors[405]);
+});
+
+router.all("/messages", (req, res) => {
+    if (req.method !== "POST") return res.status(errors[405].code).send(errors[405]);
+});
+
 router.all("*", (req, res) => {
     return res.status(errors[404].code).send(errors[404]);
 });
